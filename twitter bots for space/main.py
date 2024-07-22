481f4bd71b9577a30c0efb141d05c50f32486571
QUADRI BASIT AYOMIDE <115375709+ScriptPythonic@users.noreply.github.com>
2024-07-22 23:07:21 +0100

    change the port only

--- twitter bots for space/main.py
+++ twitter bots for space/main.py
@@ -5,7 +5,7 @@ app = create_app()
 if __name__ == '__main__':
 
     app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=4000) 
+    app.run(debug=False, host='0.0.0.0', port=5000) 
 
 
    
